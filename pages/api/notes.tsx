4168475f7003f5c42d2fc33956e9c3594e887938
David Schütz <xdavidhu@users.noreply.github.com>
2024-02-03 20:27:12 +0000

    improve subtitle view

--- pages/api/notes.tsx
+++ pages/api/notes.tsx
@@ -1,13 +1,9 @@
 import { ImageResponse } from '@vercel/og'
-import { NextRequest } from 'next/server'
+import { ImageResponseOptions, NextRequest } from 'next/server'
 
 export const config = {
-  runtime: 'experimental-edge',
+  runtime: 'edge',
 }
-
-// const fontSubtitle = fetch(new URL('../../assets/EBGaramond-Medium.ttf', import.meta.url)).then(
-//   (res) => res.arrayBuffer()
-// )
 
 const font = fetch(new URL('../../assets/EBGaramond-Bold.ttf', import.meta.url)).then(
   (res) => res.arrayBuffer()
@@ -17,60 +13,52 @@ export default async function handler(req: NextRequest) {
   try {
     const { searchParams } = new URL(req.url)
     const fontData = await font
-    // const fontSubtitleData = await fontSubtitle
 
     const hasTitle = searchParams.has('title') && searchParams.get('title').trim().length > 0
     const title = searchParams.get('title')?.slice(0, 200)
+    
+    const imageResponseOptions: ImageResponseOptions = {
+      width: 1200,
+      height: 630,
+      fonts: [
+        {
+          name: 'EB Garamond',
+          data: fontData,
+          style: 'normal',
+          weight: 700,
+        },
+      ],
+    }
 
-    return new ImageResponse(
-      (
-        <div
-          style={{
-            height: '100%',
-            width: '100%',
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            justifyContent: 'center',
-            backgroundColor: '#f6eee3',
-            fontFamily: 'EB Garamond'
-          }}
-        >
-          {hasTitle ? (
-            <div style={{
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-              justifyContent: 'center',
-            }}>
-              <div style={{ fontSize: 65 }}>notes.xdavidhu.me</div>
-              <div style={{ fontSize: 90, marginTop: 10, marginLeft: 40, marginRight: 40, textAlign: 'center', lineHeight: '92%' }}>{title}</div>
-            </div>
-          ) : (
-            <div style={{ fontWeight: 700, fontSize: 110 }}>notes.xdavidhu.me</div>
-          )}
-        </div>
-
-      ),
-      {
-        width: 1200,
-        height: 630,
-        fonts: [
-          {
-            name: 'EB Garamond',
-            data: fontData,
-            style: 'normal',
-            weight: 700,
-          },
-          // {
-          //   name: 'EB Garamond',
-          //   data: fontSubtitleData,
-          //   style: 'normal',
-          //   weight: 500,
-          // },
-        ],
-      }
-    )
+    return (hasTitle ? new ImageResponse(
+      <div style={{
+        height: '100%',
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'flex-start',
+        justifyContent: 'center',
+        backgroundColor: '#f6eee3',
+        fontFamily: 'EB Garamond',
+        padding: 50,
+      }}>
+        <div style={{ fontSize: 55 }}>notes.xdavidhu.me</div>
+        <div style={{ fontSize: 95, marginTop: 25, textAlign: 'left', lineHeight: '83%' }}>{title}</div>
+      </div>, imageResponseOptions
+    ) : new ImageResponse(
+      <div style={{
+        height: '100%',
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#f6eee3',
+        fontFamily: 'EB Garamond'
+      }}>
+        <div style={{ fontWeight: 700, fontSize: 110 }}>notes.xdavidhu.me</div>
+      </div>, imageResponseOptions
+    ))
   } catch (e: any) {
     console.log(`${e.message}`)
     return new Response(`Failed to generate the image`, {
